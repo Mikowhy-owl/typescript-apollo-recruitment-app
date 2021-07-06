5a5a609cf1a87d5d76fa0ba505da35862e963692
Mikowhy-owl <m.sowa96@gmail.com>
2021-07-06 15:21:02 +0200

    fix: wrong linking to homepage

--- src/views/NoPage.tsx
+++ src/views/NoPage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Col, Container, Row } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const NoPage = () => {
   return (
     <Container>
       <Row className="py-5 my-5">
         <Col className="text-center">
-          Page not found. Go back to <a href="/">homepage</a> .
+          Page not found. Go back to <Link to="/">homepage </Link> .
         </Col>
       </Row>
     </Container>
